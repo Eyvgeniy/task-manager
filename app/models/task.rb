b69f1c5cecfd794f2a4e75263523d5e6f381ed4b
Evgeniy <antonm33@yandex.ru>
2021-04-27 14:22:33 +0200

    feat: added state machine to task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,25 @@
 class Task < ApplicationRecord
+  state_machine initial: :new_task do
+    event :to_dev do
+      transition new_task: :in_development, in_qa: :in_development, in_code_review: :in_development
+    end
+    event :to_archive do
+      transition new_task: :archived, released: :archive
+    end
+    event :testing do
+      transition in_development: :in_qa
+    end
+    event :to_review do
+      transition in_qa: :in_code_review
+    end
+    event :to_code_release do
+      transition in_code_review: :ready_for_release
+    end
+    event :to_released do
+      transition ready_for_release: :released
+    end
+  end
+
   belongs_to :author, class_name: 'User'
   belongs_to :assignee, class_name: 'User', optional: true
 
