bf9aacc38eb0a4c5a755733a8a61cee05e1d7b7f
Evgeniy <antonm33@yandex.ru>
2021-05-13 13:26:09 +0200

    feat: added react kanban to board controller

--- app/controllers/web/boards_controller.rb
+++ app/controllers/web/boards_controller.rb
@@ -1,5 +1,9 @@
 class Web::BoardsController < Web::ApplicationController
   before_action :authenticate_user!
 
-  def show; end
+  def show
+    # puts render.inspect
+    puts "CHECKING!!!!!!!!!!"
+    render react_component: 'TaskBoard', props: {}
+  end
 end
