12bce1a621a342509e13bb2970896ce7f7241430
Evgeniy <antonm33@yandex.ru>
2021-05-16 11:57:34 +0200

    fix: remove puts from controller

--- app/controllers/web/boards_controller.rb
+++ app/controllers/web/boards_controller.rb
@@ -2,8 +2,6 @@ class Web::BoardsController < Web::ApplicationController
   before_action :authenticate_user!
 
   def show
-    # puts render.inspect
-    puts "CHECKING!!!!!!!!!!"
     render react_component: 'TaskBoard', props: {}
   end
 end
