efe73f673ce8516e2218432183020ac63a254f77
Evgeniy <antonm33@yandex.ru>
2021-05-20 21:32:29 +0200

    feat added Form to popups

--- app/javascript/components/EditPopup/EditPopup.js
+++ app/javascript/components/EditPopup/EditPopup.js
@@ -12,7 +12,7 @@ import IconButton from '@material-ui/core/IconButton';
 import Modal from '@material-ui/core/Modal';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
-import Form from './Form';
+import Form from 'components/Form';
 
 import useStyles from './useStyles';
 
